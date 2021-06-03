c4f681bc10f827a4cf04c52f29443b0909375046
Your Name <you@example.com>
2021-06-03 11:51:02 +0530

    Day7FirstCommit

--- Day6_01-06-2021/expressinternship/routes/index.js
+++ Day6_01-06-2021/expressinternship/routes/index.js
@@ -17,4 +17,52 @@ router.get('/about', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Express' });
 });
+
+router.get('/myform', function(req, res, next) {
+  res.render('form', { title: 'Express' });
+});
+
+router.post('/form-process', function(req, res, next) {
+  var num1 = parseInt(req.body.txt1);
+  var num2 = parseInt(req.body.txt2);
+
+  var c = num1+num2;
+  var msg="";
+  var mycolor=""
+
+  if(c>100){
+    msg = "You are passed"
+    mycolor = "green"
+  }
+  else{
+    msg = "You are failed"
+    mycolor = "red"
+  }
+
+
+  res.render('form-process', {'num1':num1,'num2':num2,'c':c,msg,mycolor});
+});
+
+
+router.get('/signUp', function(req, res, next) {
+  res.render('signUp', { title: 'Express' });
+});
+
+router.post('/register',function(req,res,next){
+  var {email,psw,pswrepeat,name,city,phone,pincode} = req.body;
+  var msg="";
+  var isErr = true;
+  if(psw === pswrepeat)
+  {
+    isErr=false;
+    msg="You are registered successfully";
+  }
+  else{
+    isErr=true;
+    msg="Password didn't match";
+  }
+  res.render("register",{email,psw,pswrepeat,msg,isErr,name,city,phone,pincode})
+});
+
+
 module.exports = router;
--- Day7_02_06_2021/README.md
+++ Day7_02_06_2021/README.md
@@ -9,3 +9,5 @@ Training Day 7
 ### Form Integration
 - [X] Making a simple web form
 - [X] Add SignUp Page
+
+### check Day6_01-06-2021 to see day 7 work as the only one project we are modifying things. 
